ac81e7a3671d16f659a474c4a08bbdcb135a31ce
namnguyen26399 <namnguyen26399@gmail.com>
2022-04-22 14:01:30 +0700

    uadate

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,17 +1,27 @@
 <template>
-    <div id="Project" class="overflow-scroll w-full h-screen">
+    <div id="Project" class="overflow-scroll w-full h-screen m-3 bg-white ">
         <div class="">
             <div class="ml-3 mt-3">
                 <span>1</span>
                 <span> : </span>
                 <span>Phần mềm quản lý giáo dục </span>
-                <span>12/12/2018</span>
-                <div><span>Đối tác : </span>
-                <span>Trường đại học Công nghiệp Hà Nội</span></div>
-                <div><img src="../assets/prj1.jpg.crdownload" alt="" /></div>
-                <div>
-                   
+                <span> ( 12/12/2018 )</span>
+                <div class="mt-3">
+                    <span class="ml-5">Đối tác : </span>
+                    <span>Trường đại học Công nghiệp Hà Nội</span>
                 </div>
+                <div class=" mt-5" ><img class="max-w-[600px] max-h-[400px] mx-auto" src="../assets/prj1.jpg.crdownload" alt="" /></div>
+                <textarea name="" id="" cols="30" rows="10" class="w-[90%] mx-auto mb-10 h-auto bg-white" disabled >
+                    Để xây dựng một mẫu file Excel quản lý thu chi công ty hay cá nhân hiệu quả, người dùng cần có những nội dung sau trong file:
+                        -Thông tin về chứng từ: Số thứ tự, ngày tháng (đối với thu chi công ty)
+                        -Thông tin về giao dịch: Họ và tên người thực hiện, nội dung giao dịch (đối với thu chi công ty)
+                        -Thông tin về tài khoản phát sinh: Thu/chi, nội dung, số tiền, ghi chú (nếu có)
+                    Để xây dựng một mẫu file Excel quản lý thu chi công ty hay cá nhân hiệu quả, người dùng cần có những nội dung sau trong file:
+                        -Thông tin về chứng từ: Số thứ tự, ngày tháng (đối với thu chi công ty)
+                        -Thông tin về giao dịch: Họ và tên người thực hiện, nội dung giao dịch (đối với thu chi công ty)
+                        -Thông tin về tài khoản phát sinh: Thu/chi, nội dung, số tiền, ghi chú (nếu có)
+                </textarea>
+                <div></div>
             </div>
         </div>
     </div>
@@ -21,5 +31,9 @@
 export default {};
 </script>
 
-<style>
+<style scoped>
+span{
+    font-size: 18px;
+    font-weight: 500;
+}
 </style>
