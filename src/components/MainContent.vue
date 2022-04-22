1708b6f50d6af13d4294c252e2de7764a20190ce
khoa.nguyen <khoanguyen190299@gmail.com>
2022-04-22 15:54:48 +0700

    update table list staff

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="w-full bg-blue-200">
-        <Header></Header>
-        <div class="w-full h-[50px] border-t-2 bg-white pl-3 font-medium text-base leading-[50px]">Các dự án của công ty</div>
+        <!-- <Header></Header>
+        <div class="w-full h-[50px] border-t-2 bg-white pl-3 font-medium text-base leading-[50px]">Các dự án của công ty</div> -->
         <!-- <router-view class="w-full h-full"></router-view> -->
 
-        <Project></Project>
-        <Staff />
+        <!-- <Project></Project> -->
+        <!-- <Staff /> -->
         <ListStaff />
     </div>
 </template>
